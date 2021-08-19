bcab904a9f0f463302535c1e4ddf22115b1553a3
Mihir Dalal <58912391+mihirdalal7226@users.noreply.github.com>
2021-08-19 22:55:56 +0530

    Update segmenttree.h

--- segmenttree.h
+++ segmenttree.h
@@ -27,7 +27,7 @@
 #include <ctime>
 using namespace std;
 #define int long long
-const int pinf=2e18;
+const int pinf = 2e18;
 int gcd(int a, int b)
 {
     if (b == 0)
@@ -36,6 +36,92 @@ int gcd(int a, int b)
     }
     return (b, a % b);
 }
+
+template <class T>
+class segmenttree
+{
+private:
+    int n;
+    T valueForNodes;
+    vector<T> st, a;
+    T(*combinerFunction) (T x, T y);
+
+public:
+    segmenttree(int n, vector<T> a, T val, T *(combinerFunction)(T x, T y))
+    {
+        this->n = n;
+        this->a = a;
+        this->valueForNodes = val;
+        this->st = vector<int>(4 * n + 10, val);
+        this->combinerFunction = combinerFunction;
+        buildtree(1, 1, n, a);
+    }
+    T query(int qs, int qe)
+    {
+        return query1(1, 1, n, qs, qe, a);
+    }
+    void update(int idx, T val)
+    {
+        update1(1, 1, n, idx, val, a);
+    }
+
+private:
+    void buildtree(int si, int ss, int se, vector<T> &a)
+    {
+        if (ss > se)
+        {
+            return;
+        }
+        if (ss == se)
+        {
+            st[si] = a[ss];
+            return;
+        }
+        int mid = ss + (se - ss) / 2;
+        buildtree(2 * si, ss, mid, a);
+        buildtree(2 * si + 1, mid + 1, se, a);
+        st[si] = combinerFunction(st[2 * si], st[2 * si + 1]);
+    }
+    template <class T>
+    query1(int si, int ss, int se, int qs, int qe, vector<T> &a)
+    {
+        if (ss > se)
+        {
+            return this->valueForNodes;
+        }
+        if (ss > qe || se < qs)
+        {
+            return this->valueForNodes;
+        }
+        if (ss >= qs && se <= qe)
+        {
+            return st[si];
+        }
+        int mid = (ss + se) / 2;
+        return combinerFunction(query1(2 * si, ss, mid, qs, qe, a), query1(2 * si + 1, mid + 1, se, qs, qe, a));
+    }
+    void update1(int si, int ss, int se, int idx, T val, vector<int> &a)
+    {
+        if (ss > se)
+        {
+            return;
+        }
+        if (ss > idx || se < idx)
+        {
+            return;
+        }
+        if (ss == se)
+        {
+            st[si] = val;
+            return;
+        }
+        int mid = (ss + se) / 2;
+        update1(2 * si, ss, mid, idx, val, a);
+        update1(2 * si + 1, mid + 1, se, idx, val, a);
+        st[si] = combinerFunction(st[2 * si], st[2 * si + 1]);
+    }
+};
+
 template <class T>
 class sumsegtree
 {
@@ -64,7 +150,7 @@ public:
 private:
     void build1(int si, int ss, int se, vector<T> &a)
     {
-        if(ss>se)
+        if (ss > se)
             return;
         if (ss == se)
         {
@@ -78,7 +164,7 @@ private:
     }
     T query1(int si, int ss, int se, int qs, int qe, vector<T> &a)
     {
-        if(ss>se)
+        if (ss > se)
             return 0;
         if (lazy[si] != 0)
         {
@@ -104,7 +190,7 @@ private:
     }
     void update1(int si, int ss, int se, int qs, int qe, T val, vector<T> &a)
     {
-        if(ss>se)
+        if (ss > se)
             return;
         if (lazy[si] != 0)
         {
@@ -143,13 +229,12 @@ class minsegtree
 {
 private:
     int n;
-    vector<T> st, lazy, a;
+    vector<T> st, a;
 
 public:
     minsegtree(int n, vector<T> a)
     {
         st = vector<T>(4 * n + 5, 0);
-        lazy = vector<T>(4 * n + 5, pinf);
         this->n = n;
         this->a = a;
         build1(1, 1, n, a);
@@ -158,15 +243,15 @@ public:
     {
         return query1(1, 1, n, qs, qe, a);
     }
-    void update(int qs, int qe, T val)
+    void update(int idx, T val)
     {
-        return update1(1, 1, n, qs, qe, val, a);
+        return update1(1, 1, n, idx, val, a);
     }
 
 private:
     void build1(int si, int ss, int se, vector<T> &a)
     {
-        if(ss>se)
+        if (ss > se)
             return;
         if (ss == se)
         {
@@ -180,62 +265,27 @@ private:
     }
     T query1(int si, int ss, int se, int qs, int qe, vector<T> &a)
     {
-        if(ss>se)
+        if (ss > se)
             return pinf;
-        if (lazy[si] != pinf)
-        {
-            T dx = lazy[si];
-            lazy[si] = pinf;
-            st[si] = min(st[si], dx);
-            if (ss != se)
-            {
-                lazy[2 * si] = dx;
-                lazy[2 * si + 1] = dx;
-            }
-        }
-        if (ss > qe || se < qs)
-        {
-            return pinf;
-        }
-        if (ss >= qs && se <= qe)
+        if (ss >= se && se <= qe)
         {
             return st[si];
         }
         int mid = (ss + se) / 2;
         return min(query1(2 * si, ss, mid, qs, qe, a), query1(2 * si + 1, mid + 1, se, qs, qe, a));
     }
-    void update1(int si, int ss, int se, int qs, int qe, T val, vector<T> &a)
+    void update1(int si, int ss, int se, int idx, T val, vector<T> &a)
     {
-        if(ss>se)
+        if (ss > se)
             return;
-        if (lazy[si] != pinf)
+        if (ss == se)
         {
-            T dx = lazy[si];
-            lazy[si] = pinf;
-            st[si] = min(st[si], dx);
-            if (ss != se)
-            {
-                lazy[2 * si] = dx;
-                lazy[2 * si + 1] = dx;
-            }
-        }
-        if (ss > qe || se < qs)
-        {
-            return;
-        }
-        if (ss >= qs && se <= qe)
-        {
-            st[si] = min(st[si], val);
-            if (ss != se)
-            {
-                lazy[2 * si] = val;
-                lazy[2 * si + 1] = val;
-            }
+            st[si] = val;
             return;
         }
         int mid = (ss + se) / 2;
-        update1(2 * si, ss, mid, qs, qe, val, a);
-        update1(2 * si + 1, mid + 1, se, qs, qe, val, a);
+        update1(2 * si, ss, mid, idx, val, a);
+        update1(2 * si + 1, mid + 1, se, idx, val, a);
         st[si] = min(st[2 * si], st[2 * si + 1]);
     }
 };
@@ -245,13 +295,12 @@ class maxsegtree
 {
 private:
     int n;
-    vector<T> st, lazy, a;
+    vector<T> st, a;
 
 public:
     maxsegtree(int n, vector<T> a)
     {
         st = vector<T>(4 * n + 5, 0);
-        lazy = vector<T>(4 * n + 5, -pinf);
         this->n = n;
         this->a = a;
         build1(1, 1, n, a);
@@ -260,16 +309,16 @@ public:
     {
         return query1(1, 1, n, qs, qe, a);
     }
-    void update(int qs, int qe, int val)
+    void update(int idx, int val)
     {
-        return update1(1, 1, n, qs, qe, val, a);
+        return update1(1, 1, n, idx, val, a);
     }
 
 private:
     void build1(int si, int ss, int se, vector<int> &a)
     {
-        if(ss>se)
-            return ;
+        if (ss > se)
+            return;
         if (ss == se)
         {
             st[si] = a[ss];
@@ -282,19 +331,8 @@ private:
     }
     T query1(int si, int ss, int se, int qs, int qe, vector<T> &a)
     {
-        if(ss>se)
+        if (ss > se)
             return -pinf;
-        if (lazy[si] != -pinf)
-        {
-            T dx = lazy[si];
-            lazy[si] = -pinf;
-            st[si] = max(st[si], dx);
-            if (ss != se)
-            {
-                lazy[2 * si] = dx;
-                lazy[2 * si + 1] = dx;
-            }
-        }
         if (ss > qe || se < qs)
         {
             return INT_MIN;
@@ -306,33 +344,17 @@ private:
         int mid = (ss + se) / 2;
         return max(query1(2 * si, ss, mid, qs, qe, a), query1(2 * si + 1, mid + 1, se, qs, qe, a));
     }
-    void update1(int si, int ss, int se, int qs, int qe, T val, vector<T> &a)
+    void update1(int si, int ss, int se, int idx, T val, vector<T> &a)
     {
-        if(ss>se)
-            return ;
-        if (lazy[si] != -pinf)
-        {
-            T dx = lazy[si];
-            lazy[si] = -pinf;
-            st[si] = max(st[si], dx);
-            if (ss != se)
-            {
-                lazy[2 * si] = dx;
-                lazy[2 * si + 1] = dx;
-            }
-        }
-        if (ss > qe || se < qs)
+        if (ss > se)
+            return;
+        if (ss > idx || se < idx)
         {
             return;
         }
-        if (ss >= qs && se <= qe)
+        if (ss == se)
         {
-            st[si] = max(st[si], val);
-            if (ss != se)
-            {
-                lazy[2 * si] = val;
-                lazy[2 * si + 1] = val;
-            }
+            st[idx] = val;
             return;
         }
         int mid = (ss + se) / 2;
@@ -346,13 +368,12 @@ class gcdsegtree
 {
 private:
     int n;
-    vector<int> st, lazy, a;
+    vector<int> st, a;
 
 public:
     gcdsegtree(int n, vector<int> a)
     {
         st = vector<int>(4 * n + 15, 0);
-        lazy = vector<int>(4 * n + 15, -pinf);
         this->n = n;
         this->a = a;
         build1(1, 1, n, a);
@@ -361,16 +382,16 @@ public:
     {
         return query1(1, 1, n, qs, qe, a);
     }
-    void update(int qs, int qe, int val)
+    void update(int idx, int val)
     {
-        return update1(1, 1, n, qs, qe, val, a);
+        return update1(1, 1, n, idx, val, a);
     }
 
 private:
     void build1(int si, int ss, int se, vector<int> &a)
     {
-        if(ss>se)
-            return ;
+        if (ss > se)
+            return;
         if (ss == se)
         {
             st[si] = a[ss];
@@ -383,19 +404,8 @@ private:
     }
     int query1(int si, int ss, int se, int qs, int qe, vector<int> &a)
     {
-        if(ss>se)
+        if (ss > se)
             return 0;
-        if (lazy[si] != -pinf)
-        {
-            int dx = lazy[si];
-            lazy[si] = -pinf;
-            st[si] = gcd(st[si], dx);
-            if (ss != se)
-            {
-                lazy[2 * si] = dx;
-                lazy[2 * si + 1] = dx;
-            }
-        }
         if (ss > qe || se < qs)
         {
             return 0;
@@ -407,38 +417,22 @@ private:
         int mid = (ss + se) / 2;
         return gcd(query1(2 * si, ss, mid, qs, qe, a), query1(2 * si + 1, mid + 1, se, qs, qe, a));
     }
-    void update1(int si, int ss, int se, int qs, int qe, int val, vector<int> &a)
+    void update1(int si, int ss, int se, int idx, int val, vector<int> &a)
     {
-        if(ss>se)
-            return ;
-        if (lazy[si] != -pinf)
-        {
-            int dx = lazy[si];
-            lazy[si] = -pinf;
-            st[si] = gcd(st[si], dx);
-            if (ss != se)
-            {
-                lazy[2 * si] = dx;
-                lazy[2 * si + 1] = dx;
-            }
-        }
-        if (ss > qe || se < qs)
+        if (ss > se)
+            return;
+        if (ss > idx || se < idx)
         {
             return;
         }
-        if (ss >= qs && se <= qe)
+        if (ss == idx && se == idx)
         {
-            st[si] = gcd(st[si], val);
-            if (ss != se)
-            {
-                lazy[2 * si] = val;
-                lazy[2 * si + 1] = val;
-            }
+            st[si] = val;
             return;
         }
         int mid = (ss + se) / 2;
-        update1(2 * si, ss, mid, qs, qe, val, a);
-        update1(2 * si + 1, mid + 1, se, qs, qe, val, a);
+        update1(2 * si, ss, mid, idx, val, a);
+        update1(2 * si + 1, mid + 1, se, idx, val, a);
         st[si] = gcd(st[2 * si], st[2 * si + 1]);
     }
 };
